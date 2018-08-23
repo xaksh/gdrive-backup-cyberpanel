8dfd562a3aba5c5b0f6b4093cdc215387398f9d6
Deepak Sharma <techexhibeo@gmail.com>
2018-08-23 22:36:39 +0530

    added indications for better logging.

--- backup.sh
+++ backup.sh
@@ -6,24 +6,29 @@ DATE="$(date +%Y-%b-%d)"
 G_ID=""
 BACKUP_DIR="/home/backups"
 
+echo "~~~~~~~~~~~~~~ Starting BACKUP ~~~~~~~~~~~~~~"
+echo $DATE
 # Clean old Backup Directory and Journal Files and create fresh backup directory
 rm -rf /home/*/backup/* /var/log/journal/*/*.journal $BACKUP_DIR && mkdir -p "$BACKUP_DIR/$DATE"
 wait
 
 # Run CyberPanel's Local Backup Script
-python /usr/local/CyberCP/plogical/backupScheduleLocal.py
+echo "Starting CybperPanel Backup Script"
+time python /usr/local/CyberCP/plogical/backupScheduleLocal.py
 wait
 
 # Copy tar.gz backup files to directory gdrivebackup
+echo "Copying tar balls to Backup Directory"
 mv /home/*/backup/*.tar.gz "$BACKUP_DIR/$DATE"
 wait
 
 # Upload backup files to respected Directory in Google Drive
-/usr/local/bin/gdrive upload --recursive --parent $G_ID $BACKUP_DIR/$DATE
+echo "Uploading Backup tar balls to Google Drive"
+time /usr/local/bin/gdrive upload --recursive --parent $G_ID $BACKUP_DIR/$DATE
 wait
 
 # Remove backup directory
-rm -rf $BACKUP_DIR
+time rm -rf $BACKUP_DIR
 wait
 
 exit
